ee112743814a5fc491da1930ca2ac6c02041aef7
PedroDanesvara [remote origin] <danesvara@unifesspa.edu.br>
2024-07-08 14:18:06 +0000

    chore: project update

--- login/admin.py
+++ login/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from login.models import Person, Student, PosGradStudent, Professional
 
-admin.site.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ["id_name", "email", "phone", "is_deficient", "payed", "date_added"]
+    date_hierarchy = "date_added"
+
+admin.site.register(Person, PersonAdmin)
 admin.site.register(Student)
 admin.site.register(PosGradStudent)
 admin.site.register(Professional)
