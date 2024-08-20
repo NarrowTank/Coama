bc617b91a874f74378d479a39d68847415d57e82
Erickrmoreira <89481254+Erickrmoreira@users.noreply.github.com>
2024-08-20 14:12:51 -0300

    Update paymentsUtils.py

--- login/paymentsUtils.py
+++ login/paymentsUtils.py
@@ -38,30 +38,42 @@ def attPrefferences(person_type, quantity = 1):
     return preference_response["response"]["id"]
 
 def getCoursePrefferences(total):
-    
-    
-    sdk = mercadopago.SDK('APP_USR-3755386082060312-050219-532f45efc4eacac399ab6713407966b5-1792061375')
-         
-    preference_data = {
-        "items": [
-            {
-                "id":"mini-courses-0",
-                "title": "Inscrição Mini Cursos",
-                "quantity": 1,
-                "currency_id": "BRL",
-                "unit_price": total
+    try:
+        sdk = mercadopago.SDK('APP_USR-3755386082060312-050219-532f45efc4eacac399ab6713407966b5-1792061375')
+        
+        preference_data = {
+            "items": [
+                {
+                    "id": "mini-courses-0",
+                    "title": "Inscrição Mini Cursos",
+                    "quantity": 1,
+                    "currency_id": "BRL",
+                    "unit_price": total
+                },
+            ],
+            "back_urls": {
+                "success": "https://eventocoama.com.br/auth/subscription-success",
+                "failure": "https://eventocoama.com.br/auth/failure",
+                "pending": "https://eventocoama.com.br/auth/pendings"
             },
-        ],
-        "back_urls": {
-            "success": "https://eventocoama.com.br/auth/subscription-success",
-            "failure": "https://eventocoama.com.br/auth/failure",
-            "pending": "https://eventocoama.com.br/auth/pendings"
-        },
-        "auto_return": "approved",
-    }
+            "auto_return": "approved",
+        }
 
-    preference_response = sdk.preference().create(preference_data)
-    return preference_response["response"]["id"]
+
+        preference_response = sdk.preference().create(preference_data)
+
+        print("Resposta da API:", preference_response)
+        
+
+        if 'response' in preference_response and 'id' in preference_response['response']:
+            return preference_response['response']['id']
+        else:
+           
+            raise ValueError(f"A resposta da API não contém a chave 'id'. Resposta: {preference_response}")
+    
+    except Exception as e:
+        print(f"Ocorreu um erro ao criar a preferência: {e}")
+        raise
 
 def getCoursePrefferencesDesconto(total):
     
