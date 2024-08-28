8c32659d10d5aef1a87ffeb19a37d0a09ce1f85e
Erickrmoreira <89481254+Erickrmoreira@users.noreply.github.com>
2024-08-28 19:42:53 -0300

    Add files via upload

--- coama/settings.py
+++ coama/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = '/usr/local/lsws/Example/html/coama/public/static'
+STATIC_ROOT = '/usr/local/lsws/Example/html/coama/login/static', '/usr/local/lsws/Example/html/coama/home/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
