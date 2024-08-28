d1567f607d2b5b581da0df5496cbbd8d3849c4c0
Erickrmoreira <89481254+Erickrmoreira@users.noreply.github.com>
2024-08-28 18:35:33 -0300

    comentando as credenciais de serviço de email

--- coama/settings.py
+++ coama/settings.py
@@ -130,10 +130,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # CREDENCIAIS SERVIÇO EMAIL
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'your_email@gmail.com'
-EMAIL_HOST_PASSWORD = 'your_email_password'
-DEFAULT_FROM_EMAIL = 'your_email@gmail.com'
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_PORT = 587
+#EMAIL_USE_TLS = True
+#EMAIL_HOST_USER = 'your_email@gmail.com'
+#EMAIL_HOST_PASSWORD = 'your_email_password'
+#DEFAULT_FROM_EMAIL = 'your_email@gmail.com'
