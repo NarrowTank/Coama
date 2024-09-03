41a1c847ed01f1e2b6e77d6c25c27f8afcf00e89
Erickrmoreira <89481254+Erickrmoreira@users.noreply.github.com>
2024-09-03 09:37:59 -0300

    Lógicas de pagamento

--- login/paymentsUtils.py
+++ login/paymentsUtils.py
@@ -38,6 +38,44 @@ def attPrefferences(person_type, quantity = 1):
     return preference_response["response"]["id"]
 
 def getCoursePrefferences(total):
+    try:
+        sdk = mercadopago.SDK('APP_USR-3755386082060312-050219-532f45efc4eacac399ab6713407966b5-1792061375')
+        
+        preference_data = {
+            "items": [
+                {
+                    "id": "mini-courses-0",
+                    "title": "Inscrição Mini Cursos",
+                    "quantity": 1,
+                    "currency_id": "BRL",
+                    "unit_price": total
+                },
+            ],
+            "back_urls": {
+                "success": "https://eventocoama.com.br/auth/subscription-success",
+                "failure": "https://eventocoama.com.br/auth/failure",
+                "pending": "https://eventocoama.com.br/auth/pendings"
+            },
+            "auto_return": "approved",
+        }
+
+
+        preference_response = sdk.preference().create(preference_data)
+
+        print("Resposta da API:", preference_response)
+        
+
+        if 'response' in preference_response and 'id' in preference_response['response']:
+            return preference_response['response']['id']
+        else:
+           
+            raise ValueError(f"A resposta da API não contém a chave 'id'. Resposta: {preference_response}")
+    
+    except Exception as e:
+        print(f"Ocorreu um erro ao criar a preferência: {e}")
+        raise
+
+def getCoursePrefferencesDesconto(total):
     
     
     sdk = mercadopago.SDK('APP_USR-3755386082060312-050219-532f45efc4eacac399ab6713407966b5-1792061375')
@@ -46,14 +84,14 @@ def getCoursePrefferences(total):
         "items": [
             {
                 "id":"mini-courses-0",
-                "title": "Inscrição Mini Cursos",
+                "title": "Inscrição no Evento + Mini Cursos ",
                 "quantity": 1,
                 "currency_id": "BRL",
                 "unit_price": total
             },
         ],
         "back_urls": {
-            "success": "https://eventocoama.com.br/auth/success",
+            "success": "https://eventocoama.com.br/auth/payment-subscription-success",
             "failure": "https://eventocoama.com.br/auth/failure",
             "pending": "https://eventocoama.com.br/auth/pendings"
         },
@@ -62,3 +100,32 @@ def getCoursePrefferences(total):
 
     preference_response = sdk.preference().create(preference_data)
     return preference_response["response"]["id"]
+
+def getCoursePrefferencesDescontoCombo(total):
+    
+    
+    sdk = mercadopago.SDK('APP_USR-3755386082060312-050219-532f45efc4eacac399ab6713407966b5-1792061375')
+         
+    preference_data = {
+        "items": [
+            {
+                "id":"mini-courses-0",
+                "title": "COMBO de 5 pessoas: Evento + Minicursos ",
+                "quantity": 5,
+                "currency_id": "BRL",
+                "unit_price": total
+            },
+        ],
+        "back_urls": {
+            "success": "https://eventocoama.com.br/auth/payment-subscription-combo-success",
+            "failure": "https://eventocoama.com.br/auth/failure",
+            "pending": "https://eventocoama.com.br/auth/pendings"
+        },
+        "auto_return": "approved",
+    }
+
+    
+
+    preference_response = sdk.preference().create(preference_data)
+    return preference_response["response"]["id"]
+
